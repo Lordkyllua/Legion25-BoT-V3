152bfbeb0951e9d44f1e97e459e71f87017ce17f
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-26 19:30:18 -0300

    Actualizar points.js

--- utils/points.js
+++ utils/points.js
@@ -1,55 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+// Now using the new data management system from rpg.js
+// This file is maintained for backward compatibility
 
-// Simple points system
-const getPoints = (userId) => {
-    try {
-        const pointsPath = path.join(__dirname, '../points.json');
-        if (!fs.existsSync(pointsPath)) {
-            fs.writeFileSync(pointsPath, JSON.stringify({}, null, 2));
-            return 0;
-        }
-        
-        const pointsData = fs.readFileSync(pointsPath, 'utf8');
-        const points = JSON.parse(pointsData);
-        return points[userId] || 0;
-    } catch (error) {
-        return 0;
-    }
-};
-
-const addPoints = (userId, amount) => {
-    try {
-        const pointsPath = path.join(__dirname, '../points.json');
-        const pointsData = fs.readFileSync(pointsPath, 'utf8');
-        const points = JSON.parse(pointsData);
-        
-        points[userId] = (points[userId] || 0) + amount;
-        fs.writeFileSync(pointsPath, JSON.stringify(points, null, 2));
-        
-        return points[userId];
-    } catch (error) {
-        return 0;
-    }
-};
-
-const removePoints = (userId, amount) => {
-    try {
-        const pointsPath = path.join(__dirname, '../points.json');
-        const pointsData = fs.readFileSync(pointsPath, 'utf8');
-        const points = JSON.parse(pointsData);
-        
-        points[userId] = Math.max((points[userId] || 0) - amount, 0);
-        fs.writeFileSync(pointsPath, JSON.stringify(points, null, 2));
-        
-        return points[userId];
-    } catch (error) {
-        return 0;
-    }
-};
+const { getPoints, addPoints, removePoints } = require('./rpg');
 
 module.exports = {
     getPoints,
-    addPoints,
+    addPoints, 
     removePoints
 };
