406e938e7cd950d27571665788e15181b1a9ceff
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-28 23:12:24 -0300

    Actualizar guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,18 +1,4 @@
-const pointsUtil = require('../utils/points');
-const { EmbedBuilder } = require('discord.js');
-
 module.exports = {
-  name: 'guildMemberAdd',
-  async execute(member, client) {
-    // Usar logger desde client
-    const logger = client.logger;
-    
-    // Give welcome points
-    pointsUtil.addPoints(member.id, 100);
-    
-    logger.info(`New member joined: ${member.user.tag} in ${member.guild.name}`);
-    
-    const welcomeChannel = member.guild.systemChannel;module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
         const channel = member.guild.systemChannel;
@@ -24,39 +10,4 @@ module.exports = {
             }
         }
     },
-};
-    if (!welcomeChannel) return;
-
-    const welcomeEmbed = new EmbedBuilder()
-      .setColor(0x00ff99)
-      .setTitle('🎉 Welcome to the Survival Community!')
-      .setDescription(`We're thrilled to have you, ${member.user.username}!`)
-      .setThumbnail(member.user.displayAvatarURL())
-      .addFields(
-        {
-          name: '🎁 Starter Gift',
-          value: 'You received **100 survival points** to get started!',
-          inline: true
-        },
-        {
-          name: '⚔️ Get Started',
-          value: 'Use `/help` to see all available commands and begin your adventure!',
-          inline: true
-        }
-      )
-      .setFooter({
-        text: 'Survivor Bot • Developed by LordK • Inspired by Tiny Survivors',
-        iconURL: client.user.displayAvatarURL()
-      })
-      .setTimestamp();
-
-    try {
-      await welcomeChannel.send({ 
-        content: `👋 Welcome ${member.user}!`,
-        embeds: [welcomeEmbed] 
-      });
-    } catch (error) {
-      logger.warning(`Could not send welcome message in ${member.guild.name}`);
-    }
-  }
 };
