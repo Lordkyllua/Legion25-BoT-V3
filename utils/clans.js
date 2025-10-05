310aac8f698fb02635f71421dbfaa80880319c33
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-10-04 23:26:24 -0300

    Delete utils directory

--- utils/clans.js
+++
@@ -1,9 +0,0 @@
-// Clan system - To be implemented in future versions
-
-class ClanSystem {
-    constructor() {
-        // Clan functionality will be added here
-    }
-}
-
-module.exports = ClanSystem;
--- utils/points.js
+++
@@ -1,9 +0,0 @@
-// This file is maintained for compatibility
-// Use gold.js instead for new features
-
-const { getGold, addGold } = require('./gold');
-
-module.exports = {
-    getPoints: getGold,
-    addPoints: addGold
-};
